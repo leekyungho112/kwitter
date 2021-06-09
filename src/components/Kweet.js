323d165a557f22826e4a3e33a5d1dab9385caf09
kyungho <epsoe@naver.com>
2021-06-09 14:45:57 +0900

    fiexd

--- src/components/Kweet.js
+++ src/components/Kweet.js
@@ -74,7 +74,6 @@ const Kweet = ({ kweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{kweetObj.text}</h4>
-          <h6>{kweetObj.createdAt}</h6>
           {kweetObj.attachmentUrl && (
             <img
               src={kweetObj.attachmentUrl}
